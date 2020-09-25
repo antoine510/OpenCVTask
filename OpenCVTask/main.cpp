feb96962fadee29333d8277fb5ba465858139fd2
Antoine RICHARD <antoine.richard@starburst-computing.com>
2020-09-25 11:34:51 +0200

    Updated SatLib

--- OpenCVTask/main.cpp
+++ OpenCVTask/main.cpp
@@ -15,6 +15,7 @@
 #include <nebulanetwork.h>
 #include <engine.h>
 
+#define checkAPI(expression) { int apiResult = expression; assert(apiResult >= 0); }
 
 void CALLBACK_CONV logCB(const char* msg) {
 	std::cout << msg << std::endl;
@@ -48,6 +49,7 @@ void CALLBACK_CONV actionCB(SatLib_Component_Action action, void* aData) {
 CUdevice decodeDevice = 0;
 CUcontext decodeCtx = nullptr;
 NvDecoder* decoder = nullptr;
+std::mutex decoderMutex;
 void InitializeDecoder() {
 	cuInit(0);
 
@@ -82,30 +84,51 @@ struct {
 	std::atomic<double> MinLineLength = 100.0, MaxLineGap = 10.0;
 	std::atomic<int> CannyLow = 100, CannyHigh = 200, ThresholdHough = 5;
 } ocvParams;
-sbcCometID outLinesComet;
-sbcEltID linesElt, lineCountElt;
+sbcCometID outLinesComet, frameSlotReadyComet;
+sbcEltID linesElt, lineCountElt, srfWidthElt, srfHeightElt;
 
-std::atomic<const uint8_t*> inFrame;
-std::atomic<unsigned long> inFrameSize;
+std::mutex frameMutex;
+std::condition_variable frameCV;
 
-void processFrame() {
-	static int globalFrameNumber = 0;
-	const uint8_t* currentFrame = inFrame.exchange(nullptr, std::memory_order_acq_rel);
-	unsigned long currentSize = inFrameSize.load(std::memory_order_relaxed);
+std::vector<const uint8_t*> inFrames;
+std::vector<unsigned long> inFrameSizes;
+std::mutex frameQueueMutex;
+std::atomic<int> globalFrameNumber = 0;
+std::atomic<bool> processingFrames = true;
+std::atomic<unsigned> concurrentCount = 0;
+unsigned concurrentMax = 0;
 
+void SignalFrameSlotReady() {
+	SatLib_Nebula_SendComet("frameSlotReady", frameSlotReadyComet);
+}
+
+void processFrame(const uint8_t* frame, unsigned long frameSize) {
 	uint8_t** outFrames;
+	uint8_t* myFrame;
 	int outFrameCount = 0;
-	if(decoder->Decode(currentFrame, currentSize, &outFrames, &outFrameCount, CUVID_PKT_ENDOFPICTURE, nullptr, globalFrameNumber++) != true) {
-		logCB("Error on decode");
-		return;
+	int width = 0, height = 0;
+	auto start = std::chrono::system_clock::now();
+	{
+		std::lock_guard<std::mutex> lk(decoderMutex);	// DecodeLockFrame, GetWidth/Height are not thread safe, gate them
+		if(!decoder->DecodeLockFrame(frame, frameSize, &outFrames, &outFrameCount, CUVID_PKT_ENDOFPICTURE, nullptr, globalFrameNumber.fetch_add(1, std::memory_order_relaxed))) {
+			logCB("Error on decode");
+			return;
+		}
+		myFrame = outFrames[0];
+		width = decoder->GetWidth();
+		height = decoder->GetHeight();
 	}
+	//std::cout << "Decode time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << std::endl;
 
 	if(outFrameCount != 1) {
 		logCB(("Error received frame count: " + std::to_string(outFrameCount)).c_str());
-		return;
+		std::terminate();
 	}
 
-	cv::Mat input(decoder->GetHeight(), decoder->GetWidth(), CV_8UC1, outFrames[0]);
+	concurrentCount.fetch_add(1u, std::memory_order_release);
+
+	// Matrices do not duplicate data on creation, still using NVDecode frame
+	cv::Mat input(height, width, CV_8UC1, myFrame);
 
 	cv::Mat lines;
 	cv::Mat edges(input.rows, input.cols, CV_8UC1);
@@ -116,15 +139,25 @@ void processFrame() {
 					ocvParams.MinLineLength.load(std::memory_order_relaxed),
 					ocvParams.MaxLineGap.load(std::memory_order_relaxed));
 
-	SatLib_Buffer outbuf{(char*)lines.data, (uint32_t)(lines.dataend - lines.datastart)};
-	SatLib_Comet_SetInteger(lineCountElt, lines.rows);
-	SatLib_Comet_SetBuffer(linesElt, outbuf);
+	// Lines are extracted, we no longer use NVDecode data, Unlock frame is thread safe
+	decoder->UnlockFrame(&myFrame, 1);
 
-	/*int buf[] = {0, 0, 1024, 1024, 1024, 0, 0, 1024};
-	SatLib_Comet_SetInteger(lineCountElt, sizeof(buf) >> 4);
-	SatLib_Comet_SetBuffer(linesElt, SatLib_Buffer{(const char*)buf, sizeof(buf)});*/
+	SignalFrameSlotReady();
 
-	SatLib_Nebula_SendComet("outLines", outLinesComet);
+	if(lines.rows > 0) {
+		SatLib_Comet_SetInteger(srfWidthElt, width);
+		SatLib_Comet_SetInteger(srfHeightElt, height);
+		SatLib_Buffer outbuf{(char*)lines.data, (uint32_t)(lines.dataend - lines.datastart)};
+		SatLib_Comet_SetInteger(lineCountElt, lines.rows);
+		SatLib_Comet_SetBuffer(linesElt, outbuf);
+
+		SatLib_Nebula_SendComet("lines", outLinesComet);
+	}
+
+	// Can be wrong (not thread safe), whatever
+	unsigned conCount = concurrentCount.load(std::memory_order_acquire);
+	if(conCount > concurrentMax) concurrentMax = conCount;
+	concurrentCount.fetch_sub(1, std::memory_order_release);
 
 	/*cv::Mat lineImage(input.rows, input.cols, CV_8UC1);
 	memset(lineImage.data, 0, lineImage.dataend - lineImage.datastart);
@@ -138,8 +171,28 @@ void processFrame() {
 	cv::waitKey(0);*/
 }
 
-std::mutex frameMutex;
-std::condition_variable frameCV;
+void processThread() {
+	std::unique_lock<std::mutex> lk(frameMutex, std::defer_lock);
+	while(processingFrames.load(std::memory_order_relaxed)) {
+		lk.lock();
+		frameCV.wait_for(lk, std::chrono::milliseconds(100));
+		lk.unlock();
+
+		const uint8_t* frame;
+		unsigned long frameSize = 0;
+		{
+			std::lock_guard<std::mutex> lk(frameQueueMutex);
+			unsigned frameSlot = 0;
+			while(frameSlot < inFrames.size() && !inFrames[frameSlot]) ++frameSlot;
+			if(frameSlot == inFrames.size()) continue;	// Anti spurious wakeup
+
+			frame = inFrames[frameSlot];
+			frameSize = inFrameSizes[frameSlot];
+			inFrames[frameSlot] = nullptr;
+		}
+		processFrame(frame, frameSize);
+	}
+}
 
 void CALLBACK_CONV frameReceived(void* ctx) {
 	sbcCometID cid;
@@ -152,12 +205,18 @@ void CALLBACK_CONV frameReceived(void* ctx) {
 	SatLib_Comet_Delete(cid);		// Does not delete underlying buffer
 
 	const uint8_t* expected = nullptr;
-	if(!inFrame.compare_exchange_strong(expected, (const uint8_t*)buf.data, std::memory_order_acq_rel)) {
-		free((void*)buf.data);
-		logCB("Dropped a frame");
-		return;
+	int frameSlot = 0;
+	{
+		std::lock_guard<std::mutex> lk(frameQueueMutex);
+		while(frameSlot < inFrames.size() && inFrames[frameSlot]) ++frameSlot;
+		if(frameSlot == inFrames.size()) {	// No place for the frame, we have to drop it
+			free((void*)buf.data);
+			logCB("Dropped a frame");
+			return;
+		}
+		inFrames[frameSlot] = reinterpret_cast<const uint8_t*>(buf.data);
+		inFrameSizes[frameSlot] = buf.size;
 	}
-	inFrameSize.store(buf.size, std::memory_order_relaxed);
 
 	frameCV.notify_one();
 }
@@ -186,6 +245,8 @@ void updateParameters(void* ctx) {
 	SatLib_Comet_AsDecimal(eid, &dv);
 	ocvParams.MaxLineGap.store(dv, std::memory_order_relaxed);
 
+	logCB("Received parameters");
+
 	SatLib_Comet_Delete(cid);
 }
 
@@ -198,17 +259,17 @@ int main() {
 
 	SatLib_Comet_InitModule();
 	SatLib_Nebula_InitModule();
-	SatLib_Engine_InitModule("OCVSat");
+	SatLib_Engine_InitModule("OCVTaskSat");
 
 	while(SatLib_Engine_IsLocalRunning() == 0) {
 		logCB("Waiting for engine connection...");
-		std::this_thread::sleep_for(1s);
+		std::this_thread::sleep_for(2s);
 	}
 
 	IGC_Satellite self;
-	if(IGC_Satellite_GetLocal("OCVSat", &self) != 0) {
-		IGC_Satellite_Deploy("starburst/OCVSat");
-		assert(IGC_Satellite_GetLocal("OCVSat", &self) == 0);
+	if(IGC_Satellite_GetLocal("OCVTaskSat", &self) != 0) {
+		IGC_Satellite_Deploy("starburst/OCVTaskSat");
+		checkAPI(IGC_Satellite_GetLocal("OCVTaskSat", &self));
 	}
 
 	auto startFuture = startPromise.get_future();
@@ -216,99 +277,81 @@ int main() {
 
 	IGC_Component ocvTaskCo;
 	IGC_Component_Future ocvTaskCoFuture;
-	SatLib_Component_Setup("starburst/OCVTask", actionCB, nullptr, &ocvTaskCoFuture);
+	SatLib_Component_Setup("starburst/OCVTaskLines", actionCB, nullptr, &ocvTaskCoFuture);
 	IGC_Component_GetByFields(ocvTaskCoFuture.name, ocvTaskCoFuture.version, &ocvTaskCo);
 
+	/* Find our session */
 	IGC_Session session;
 	while(int result = IGC_Session_FindForSat(self.id, &session) > 0) {
-		assert(result >= 0);
-		std::this_thread::sleep_for(1s);
+		checkAPI(result);
+		logCB("Waiting for session to run...");
+		std::this_thread::sleep_for(2s);
 	}
-	assert(SatLib_Engine_JoinSession(session.id, session.engineAddress) == 0);
+	checkAPI(SatLib_Engine_JoinSession(session.id, session.engineAddress));
+
+	/* Wait for Start of OCVTaskLines Component*/
+	startFuture.wait();
 
 	IGC_Pairing ocvTaskSp{0};
-	assert(SatLib_Component_GetSessionPairing(ocvTaskCo.id, &ocvTaskSp) == 0);
+	checkAPI(SatLib_Component_GetSessionPairing(ocvTaskCo.id, &ocvTaskSp) == 0);
 
+	/* Extract parameters from pairing for ease of access */
 	std::unordered_map<std::string, IGC_Component_ParamValue> parameters;
 	for(unsigned int i = 0; i < ocvTaskSp.paramCount; ++i) {
 		parameters.emplace(ocvTaskSp.paramNames[i], ocvTaskSp.paramValues[i]);
 	}
 
-	SatLib_Topic_Subscribe("taskParameters", SATLIB_TOPIC_STREAM, true, updateParameters, nullptr, &tptid);
-	SatLib_Topic_Subscribe("inFrame", SATLIB_TOPIC_STREAM, true, frameReceived, nullptr, &itid);
+	/* Topic ocvTaskLinesParams contains task parameter updates */
+	SatLib_Topic_Subscribe("ocvTaskLinesParams", SATLIB_TOPIC_STREAM, true, updateParameters, nullptr, &tptid);
 
-	/* Setup output Comet */
-	assert(SatLib_Comet_Load("starburst/OutLines", &outLinesComet) == 0);
+	/* Topic videoFrame contains the video frames to be analysed */
+	SatLib_Topic_Subscribe("videoFrame", SATLIB_TOPIC_STREAM, true, frameReceived, nullptr, &itid);
+
+	/* Setup output Comet for lines data */
+	checkAPI(SatLib_Comet_Load("starburst/Lines", &outLinesComet));
 	SatLib_Comet_Get(outLinesComet, "Lines", &linesElt);
 	SatLib_Comet_Get(outLinesComet, "LineCount", &lineCountElt);
+	SatLib_Comet_Get(outLinesComet, "SrfWidth", &srfWidthElt);
+	SatLib_Comet_Get(outLinesComet, "SrfHeight", &srfHeightElt);
 
-	startFuture.wait();
+	/* Setup output Comet for processing slot readyness signal */
+	checkAPI(SatLib_Comet_Load("starburst/FrameSlotReady", &frameSlotReadyComet));
 
 	InitializeDecoder();
 
+	unsigned threadCount = parameters.at("ThreadCount").valueUnion.int_;
+	if(threadCount == 0) threadCount = std::thread::hardware_concurrency();
+	std::cout << "Running tasks on " << threadCount << " threads" << std::endl;
+	inFrames.resize(threadCount, nullptr);
+	inFrameSizes.resize(threadCount, 0);
+
+	auto processingThreads = std::vector<std::thread>();
+
 	SatLib_Component_ReportState(ocvTaskCo.id, SATLIB_COMPONENT_STARTED);
 
-	/* Starting moonvdec */
-	/*assert(mvd_Init(logCB) == 0);
-	atexit(mvd_Close);
-
-	mvd_StreamSource src = mvd_GetStreamSource(parameters.at("GamestreamAddress").valueUnion.string);
-	assert(src != nullptr);
-
-	assert(mvd_PairStreamSource(src, "2566") == 0);
-
-	const int* ids, *lens;
-	const wchar_t* const* names;
-	int count = mvd_GetAppList(src, &ids, &names, &lens);
-	assert(count >= 0);
-
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-	auto appNameW = converter.from_bytes(parameters.at("GamestreamApp").valueUnion.string);
-	int appID = -1;
-	for(int i = 0; i < count; ++i) {
-		if(names[i] == appNameW) {
-			appID = ids[i];
-			break;
-		}
+	for(int i = 0; i < threadCount; ++i) {
+		processingThreads.emplace_back(processThread);
 	}
-	assert(appID != -1);
 
-	sconfig.width = (int)parameters.at("CaptureWidth").valueUnion.int_;
-	sconfig.height = (int)parameters.at("CaptureHeight").valueUnion.int_;
-	sconfig.fps = (int)parameters.at("RefreshRate").valueUnion.int_;
-	sconfig.bitrate = (int)parameters.at("Bitrate").valueUnion.int_ * 1000;
-	sconfig.packetSize = 1024;
-	sconfig.streamingRemotely = (int)false;
-	sconfig.audioConfiguration = AUDIO_CONFIGURATION_STEREO;
-	sconfig.supportsHevc = (int)false;
-	sconfig.enableHdr = (int)false;
-	sconfig.hevcBitratePercentageMultiplier = 75;
-	sconfig.clientRefreshRateX100 = 5994;
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-	mvd_LaunchApp(src, appID, &sconfig);
-	assert(mvd_StartStream(src, &sconfig, frameCB, nullptr) == 0);*/
+	for(int i = 0; i < threadCount; ++i) {
+		SignalFrameSlotReady();
+	}
 
-	bool processingFrames = true;
 
-	auto processThread = std::thread([&processingFrames]() {
-		std::unique_lock<std::mutex> lk(frameMutex);
-		while(processingFrames) {
-			frameCV.wait_for(lk, 50ms);
-			if(inFrame.load(std::memory_order_acquire) != nullptr) {
-				processFrame();
-			}
-		}
-									 });
+	while(stopFuture.wait_for(std::chrono::seconds(5)) != std::future_status::ready) {
+		std::cout << "Active threads: " << concurrentMax << std::endl;
+	}
 
-	stopFuture.wait();
-	processingFrames = false;
-	if(processThread.joinable()) processThread.join();
+	processingFrames.store(false, std::memory_order_relaxed);
+	for(auto& thread : processingThreads) {
+		if(thread.joinable()) thread.join();
+	}
 
 	DestroyDecoder();
 
 	SatLib_Component_ReportState(ocvTaskCo.id, SATLIB_COMPONENT_STOPPED);
-
-	//mvd_StopStream(src);
 
 	SatLib_Engine_CloseModule();
 	SatLib_Nebula_CloseModule();
